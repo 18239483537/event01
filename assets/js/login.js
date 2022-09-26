d1ae0cf312230c6e8a68b29c704ada25487319e7
Lijunjiang <18239483537@163.com>
2022-09-26 19:52:06 +0800

    初始化项目

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+$(function(){
+ $('#go2reg').on('click',function(){
+    $('.login-wrap').herf()
+    $('.reg-wrap').show()
+ })
+ $('#go2login').on('click',function(){
+    $('.login-wrap').show()
+    $('.reg-wrap').hide()
+ })
+
+})
